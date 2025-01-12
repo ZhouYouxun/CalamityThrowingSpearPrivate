c42ff2bd3aaae60394e7e78fa8bf383e2a39d601
YitaoWang12 <wangk3058@gmail.com>
2025-01-11 22:26:20 -0800

    1.0.8.1

--- obj/Debug/net8.0/CalamityThrowingSpear.AssemblyInfo.cs
+++ obj/Debug/net8.0/CalamityThrowingSpear.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalamityThrowingSpear")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84330188d90d5bdc3dd7542eab9b2aeb851d84ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalamityThrowingSpear")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalamityThrowingSpear")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
