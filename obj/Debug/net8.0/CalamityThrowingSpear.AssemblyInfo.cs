84ecfcb9d7e45dc7b9cba1470981ad8272cdba08
YitaoWang12 <wangk3058@gmail.com>
2025-01-15 12:33:18 -0800

    重做了几把武器并封装
    天底的禁止增伤被重置
    重做宇宙暗流，翻新恒辉

--- obj/Debug/net8.0/CalamityThrowingSpear.AssemblyInfo.cs
+++ obj/Debug/net8.0/CalamityThrowingSpear.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalamityThrowingSpear")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8733129d82bec4aaaa088fcabbe375e797b9e7f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9447a17f53c3763c0c7e70f52cfe937c344598ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalamityThrowingSpear")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalamityThrowingSpear")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
