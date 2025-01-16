8182a33f23e8387343246ef3a271a5c65f005354
Tokisaki kuronmi <3504002330@qq.com>
2025-01-16 22:17:51 +0800

    Update BrimlanceJav.cs

--- Weapons/ChangedWeapons/BPrePlantera/BrimlanceC/BrimlanceJav.cs
+++ Weapons/ChangedWeapons/BPrePlantera/BrimlanceC/BrimlanceJav.cs
@@ -26,7 +26,7 @@ namespace CalamityThrowingSpear.Weapons.ChangedWeapons.BPrePlantera.BrimlanceC
         {
             Item.width = 44;
             Item.height = 50;
-            Item.damage = 200; // 设置伤害值
+            Item.damage = 160; // 设置伤害值
             Item.DamageType = DamageClass.Melee; // 设置为近战武器
             Item.noMelee = true;
             Item.useTurn = true;
