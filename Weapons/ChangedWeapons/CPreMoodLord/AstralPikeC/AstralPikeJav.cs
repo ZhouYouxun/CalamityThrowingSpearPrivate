7ba58997c79dc85a6dd8b27008cc62794d70d917
Tokisaki kuronmi <3504002330@qq.com>
2025-01-18 20:17:28 +0800

    Update AstralPikeJav.cs

--- Weapons/ChangedWeapons/CPreMoodLord/AstralPikeC/AstralPikeJav.cs
+++ Weapons/ChangedWeapons/CPreMoodLord/AstralPikeC/AstralPikeJav.cs
@@ -26,7 +26,7 @@ namespace CalamityThrowingSpear.Weapons.ChangedWeapons.CPreMoodLord.AstralPikeC
         {
             Item.width = 44;
             Item.height = 50;
-            Item.damage = 234; // 设置伤害值
+            Item.damage = 300; // 设置伤害值
             Item.DamageType = DamageClass.Melee; // 设置为近战武器
             Item.noMelee = true;
             Item.useTurn = true;
