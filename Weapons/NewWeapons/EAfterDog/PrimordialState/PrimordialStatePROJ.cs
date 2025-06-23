a345d23a726a6aab01e6fdfa2d411a100fb354ab
Tokisaki kuronmi <3504002330@qq.com>
2025-06-24 07:32:24 +0800

    Update PrimordialStatePROJ.cs

--- Weapons/NewWeapons/EAfterDog/PrimordialState/PrimordialStatePROJ.cs
+++ Weapons/NewWeapons/EAfterDog/PrimordialState/PrimordialStatePROJ.cs
@@ -45,7 +45,7 @@ namespace CalamityThrowingSpear.Weapons.NewWeapons.EAfterDog.PrimordialState
             Projectile.timeLeft = 6000;
             Projectile.light = 0.5f;
             Projectile.ignoreWater = true;
-            Projectile.tileCollide = true; // 允许与方块碰撞
+            Projectile.tileCollide = false; // 允许与方块碰撞
             Projectile.extraUpdates = 2; // 额外更新次数
             Projectile.usesLocalNPCImmunity = true; // 弹幕使用本地无敌帧
             Projectile.localNPCHitCooldown = 14; // 无敌帧冷却时间为14帧
