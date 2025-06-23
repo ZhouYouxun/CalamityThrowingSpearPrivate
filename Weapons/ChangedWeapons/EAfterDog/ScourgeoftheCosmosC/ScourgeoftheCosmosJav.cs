241860c19e13d3753f21590aabed31116e9a65dc
Tokisaki kuronmi <3504002330@qq.com>
2025-06-23 12:30:17 +0800

    Update ScourgeoftheCosmosJav.cs
    灾劫宙怨面板由375降低至135，使用时间由12提高至16，使用动画由30提高至36

--- Weapons/ChangedWeapons/EAfterDog/ScourgeoftheCosmosC/ScourgeoftheCosmosJav.cs
+++ Weapons/ChangedWeapons/EAfterDog/ScourgeoftheCosmosC/ScourgeoftheCosmosJav.cs
@@ -24,14 +24,14 @@ namespace CalamityThrowingSpear.Weapons.ChangedWeapons.EAfterDog.ScourgeoftheCos
         {
             Item.width = 44;
             Item.height = 50;
-            Item.damage = 375; // 设置伤害值
+            Item.damage = 135; // 设置伤害值
             Item.DamageType = DamageClass.Melee; // 设置为近战武器
             Item.noMelee = true;
             Item.useTurn = true;
             Item.noUseGraphic = true;
             Item.useStyle = ItemUseStyleID.Swing; // 更改使用模式为投掷
-            Item.useTime = 12;
-            Item.useAnimation = 30; // 更改使用时的武器攻击速度
+            Item.useTime = 16;
+            Item.useAnimation = 36; // 更改使用时的武器攻击速度
             Item.useLimitPerAnimation = 2;
             Item.knockBack = 8.5f;
             Item.UseSound = SoundID.Item109;
