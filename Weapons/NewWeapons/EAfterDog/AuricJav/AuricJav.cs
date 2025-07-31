24549cd05bcd226794290949840f476008876ac1
Tokisaki kuronmi <3504002330@qq.com>
2025-07-31 08:01:30 +0800

    Update AuricJav.cs

--- Weapons/NewWeapons/EAfterDog/AuricJav/AuricJav.cs
+++ Weapons/NewWeapons/EAfterDog/AuricJav/AuricJav.cs
@@ -35,7 +35,7 @@ namespace CalamityThrowingSpear.Weapons.NewWeapons.EAfterDog.AuricJav
         {
             Item.width = 44;
             Item.height = 50;
-            Item.damage = 102; // 设置伤害值
+            Item.damage = 100; // 设置伤害值
             Item.DamageType = DamageClass.Melee; // 设置为近战武器
             Item.noMelee = true;
             Item.useTurn = true;
